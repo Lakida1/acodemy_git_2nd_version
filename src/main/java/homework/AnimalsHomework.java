f0bdd19aec98b6deb3d2069ea971c5942f4deca0
Lakida1 <lakida1@inbox.lv>
2022-02-10 18:10:56 +0200

    [master]:Animals Homework

---
+++ src/main/java/homework/AnimalsHomework.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class AnimalsHomework {
+    public static void main(String[] args) {
+    AnimalHomework cat = new AnimalHomework("The Cat",true);
+    AnimalHomework dog = new AnimalHomework("The Dog",false);
+    AnimalHomework bird = new AnimalHomework("The Bird",false);
+
+       cat.fly();
+       cat.fly();
+       cat.fly();
+       cat.fly();
+       cat.fly();
+       cat.fly();
+       cat.eatSecond();
+       cat.eat();
+       cat.superEat();
+
+
+
+
+
+
+    }
+}
