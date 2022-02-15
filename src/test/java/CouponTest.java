3d7b97c4a5d12332feb7d6b3031ee9702ad39ceb
Lakida1 <lakida1@inbox.lv>
2022-02-15 18:46:53 +0200

    [master]:HomePage Test 1: to find element, add product, to stay in Cart

--- src/test/java/CouponTest.java
+++ src/test/java/CouponTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -10,23 +12,38 @@ import static org.hamcrest.Matchers.containsString;
 
 public class CouponTest {
     WebDriver driver;
+
     @BeforeEach
-    public void init()
-    {    driver = new ChromeDriver();
+    public void init() {
+        driver = new ChromeDriver();
         //ChromeOptions chromeOptions = new ChromeOptions();
         //ChromeOptions.addArguments("--disable-logging");
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
     }
+
     @Test
     public void applyCouponCode() {
         driver.get("http://shop.acodemy.lv/");
-       driver.getTitle();
-       System.out.println(driver.getTitle());
-        //assertThat(driver.getTitle(),equals("Online shop – acodemy – Just another WordPress site"));
-        //assertThat(driver.getTitle(),equals("123"));
-        //assertThat(driver.getTitle(),containsString("Online Shop"));
+        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys("Album");
+        driver.findElement(By.id("woocommerce-product-search-field-0")).submit();
+        // nize ta ze samaja zapis, no prisvoen ELEMENT searhField =
+        //WebElement searchField = driver.findElement(By.id("woocommerce-product-search-field-0"));
+//        searchField.sendKeys("Album");
+
+       // zahodit na stranicu, nahodit Album, dobavlaet 1 pcs, zahodit v pole View cart:
+      WebElement searchField = driver.findElement(By.id("woocommerce-product-search-field-0"));
+       searchField.sendKeys("Album");
+       searchField.submit();
+       WebElement addToCartButton = driver.findElement(By.id("add-to-cart"));
+       addToCartButton.click();
+        WebElement viewCart = driver.findElement(By.linkText("View cart"));
+        System.out.println();
+
+
     }
-    @AfterEach
-    public void pageClose() {
-        driver.quit();}
+// zakrivaet stranicu posle operacij po testu:
+    //@AfterEach
+    //public void pageClose() {
+        //driver.quit();
+    //}
 }
