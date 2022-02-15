a91305d77bd322360740c0b155ab5836caeb5d51
Lakida1 <lakida1@inbox.lv>
2022-02-15 22:09:46 +0200

    [master]:HomePage Test3

--- src/test/java/CouponTest.java
+++ src/test/java/CouponTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import page_object.HeaderPage;
+import page_object.HomePage;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -14,24 +15,38 @@ import static org.hamcrest.Matchers.containsString;
 public class CouponTest {
     WebDriver driver;
     HeaderPage header;
+    HomePage homePage;
 
     @BeforeEach
     public void init() {
         driver = new ChromeDriver();// zdes proishodit inicializacija driver:
         HeaderPage header = new HeaderPage(driver); // sozdali object header na osnove class HeaderPage, v k-j podajom driver, k-j proincializirovan v class HeaderPage
         this.header = new HeaderPage(driver);
-
+        this.homePage = new HomePage(driver);
     }
 
     @Test
     public void applyCouponCode() {
         driver.get("http://shop.acodemy.lv/");
-        header.searForAllProduct("Beanie");
-
+        homePage.clickOnProduct("Hoodie");
+        homePage.selectColor("Green");
+        System.out.println("123");
     }
+//        homePage.getAllProducts();
+//        for (int i = 0; i < homePage.getAllProducts().size(); i++) {
+//            {if (homePage.getAllProducts().get(i).getText().contains("Cap")) {
+//                    homePage.getAllProducts().get(i).click();}}}
+
+        //analogicnaja zapis (to cto vise):
+//        for (WebElement product: homePage.getAllProducts())
+//              {if (product.getText().contains("Cap"))
+//              {product.click();}
+//                  break;}
+
+
 // zakrivaet stranicu posle operacij po testu:
     //@AfterEach
     //public void pageClose() {
-        //driver.quit();
+    //driver.quit();
     //}
 }
