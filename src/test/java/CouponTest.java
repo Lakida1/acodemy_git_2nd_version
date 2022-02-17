0c26c9d7e88d3f12f54c932d82f4bbfababdfb87
Lakida1 <lakida1@inbox.lv>
2022-02-17 18:36:57 +0200

    [master]:Test 4

--- src/test/java/CouponTest.java
+++ src/test/java/CouponTest.java
@@ -1,3 +1,5 @@
+import constants.Products;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,9 +11,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import page_object.HeaderPage;
 import page_object.HomePage;
 
+import static constants.Products.HOODIE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-
+@Slf4j
 public class CouponTest {
     WebDriver driver;
     HeaderPage header;
@@ -27,26 +30,30 @@ public class CouponTest {
 
     @Test
     public void applyCouponCode() {
+        log.info("Step 1: user open Web page");
         driver.get("http://shop.acodemy.lv/");
+
+        log.info("Step 2: User select product by product name" + HOODIE);
         homePage.clickOnProduct("Hoodie");
+
+        log.info("Step 3: user select color of product");
         homePage.selectColor("Green");
+
+        log.info("Step 4: user select a logo");
+
+        log.info("Step 5: user cancel first color");
+
+        log.info("Step 6: user select new color of product"); // net info na kakih cvetah vozmozno vibrat logo - esli netu logo, to 2-e pole dolzno stat neaktivnim
+
+        log.info("Step 7: user select quantity of product");
+
         System.out.println("123");
     }
-//        homePage.getAllProducts();
-//        for (int i = 0; i < homePage.getAllProducts().size(); i++) {
-//            {if (homePage.getAllProducts().get(i).getText().contains("Cap")) {
-//                    homePage.getAllProducts().get(i).click();}}}
-
-        //analogicnaja zapis (to cto vise):
-//        for (WebElement product: homePage.getAllProducts())
-//              {if (product.getText().contains("Cap"))
-//              {product.click();}
-//                  break;}
 
 
 // zakrivaet stranicu posle operacij po testu:
-    //@AfterEach
-    //public void pageClose() {
-    //driver.quit();
-    //}
+//    @AfterEach
+//    public void pageClose() {
+//    driver.quit();
+//    }
 }
