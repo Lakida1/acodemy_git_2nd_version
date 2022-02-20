aef1a1d319c123770282b28300c04032db834473
Lakida1 <lakida1@inbox.lv>
2022-02-20 20:22:27 +0200

    [master]:TEST 2.4 Header Page

--- src/test/java/page_object/HeaderPage.java
+++ src/test/java/page_object/HeaderPage.java
@@ -2,23 +2,25 @@ package page_object;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import utils.LocalDriverManager;
+//
 public class HeaderPage {
-    WebDriver driver;
-    //costructor:
-    public HeaderPage(WebDriver driver) {
-        this.driver = driver;
+    WebDriver driver; //   private final WebDriver driver = LocalDriverManager.getInstance();
+
+        public HeaderPage(WebDriver driver){
+            this.driver = driver;
+        }
+
+    public By searchField = By.id("woocommerce-product-search-field-0");
+    public void searchForAllProduct(String nameOfProduct){
+        driver.findElement(searchField).sendKeys(nameOfProduct);
+        driver.findElement(searchField).submit();
     }
 
-    public By searchFild = By.id("woocommerce-product-search-field-0");
-    // sozdali class HeaderPage, tam objavili driverm ctobi on zapolnilsja s pomoscju constructora, pri sozdaniji novogo object v HeaderPage, budem podavat v nego driver, k-j proinicalizirovan
-    //ukazali element i to kak budem ego nahodit (po ID)
-    //sozdali Method, v k-j podajom nazvanije producta i v etom metode nahodim etot element (search.Field) i otpravlaem tuda imja producta, k-e prihodit iz classa, v k-j podaem eto znacenije
-    //v etom ze metode clikaem na najdennij elemnt
 
-    public void searForAllProduct(String nameOfProduct){
-        driver.findElement(searchFild).sendKeys(nameOfProduct);
-        driver.findElement(searchFild).submit();
-    }
 }
